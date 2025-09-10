49bc0433d82e33fc54cf5320c093f87438ae6f92
ness <shonness98@gmail.com>
2025-09-11 02:56:55 +0500

    Reorder router registration to maintain consistency.

--- app/bot/handlers/__init__.py
+++ app/bot/handlers/__init__.py
@@ -9,10 +9,9 @@ logger = logging.getLogger(__name__)
 
 
 def register(dp: Dispatcher) -> None:
-    dp.include_router(private.router)
-
     dp.include_router(group.router)
     dp.include_router(group.id_router)
+    dp.include_router(private.router)
 
     logger.info("Handlers registered")
 
